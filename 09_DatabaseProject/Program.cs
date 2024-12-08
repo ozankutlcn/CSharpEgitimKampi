125dcb079c9901392e59f28d8a0313fadc619260
Ozan Kutlucan <ozan_kutlucan_@hotmail.com>
2024-12-08 21:48:44 +0300

    09_DatabaseProject
    09_DatabaseProject

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ado .NET (C#'da SQL yapılarını kullanmamızı sağlayan bir araç)
+            Console.WriteLine("***** C# Veri Tabanlı Ürün Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen Getirmek İstediğiniz Tablo Numarasını Giriniz");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=Ozan;initial Catalog=EgitimKampiDB;integrated security=true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * from TblCategory",connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows) //DataRow,DataTable içerisindeki satırları sana sunar.
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(" " + item.ToString());
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
